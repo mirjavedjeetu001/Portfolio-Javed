a695df8850f89385a58cfb0fd0ea9e5a71983d3c
mirjavedjeetu <javed@iamsheba.xyz>
2025-11-03 10:46:58 +0600

    first commit

---
+++ application/config/database.example.php
@@ -0,0 +1,26 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+// Database Configuration Template
+// Copy this file to database.php and update with your credentials
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'your_database_username';
+$db['default']['password'] = 'your_database_password';
+$db['default']['database'] = 'your_database_name';
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = FALSE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
+
+/* End of file database.php */
+/* Location: ./application/config/database.php */
